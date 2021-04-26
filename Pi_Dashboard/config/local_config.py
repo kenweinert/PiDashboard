6edfba7efb878f0732ce098e34b3a21d068729af
Ken Weinert <kweinert@gmail.com>
2021-04-26 15:00:32 -0600

    feat/FlaskSetup set up Flask to run the application

---
+++ Pi_Dashboard/config/local_config.py
@@ -0,0 +1,13 @@
+# Local Development config file
+# https://flask.palletsprojects.com/en/1.1.x/config/
+
+# Flask-specific values
+TESTING = True
+APPLICATION_ROOT = "/"
+PREFERRED_URL_SCHEME = "http"
+
+# Custom values
+# Possible logging levels:
+# CRITICAL - FATAL - ERROR - WARNING - INFO- NOTSET
+LOGGER_LEVEL = "DEBUG"
+LOG_FILE_NAME = "PiDashboard.log" 

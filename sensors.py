d06ff8e371a22d6c245a72162b83cd213230fb3f
Ken Weinert <kweinert@gmail.com>
2021-04-26 18:11:13 -0600

    feat/temperature having an issue with importing from .data

---
+++ sensors.py
@@ -0,0 +1,33 @@
+from flask import current_app as app
+from builtins import staticmethod
+from w1thermsensor.core import convert_raw_temperature_to_sensor_count
+
+try:
+    from w1thermsensor  import W1ThermSensor
+except Exception:
+    W1ThermSensor = None
+    
+class Sensors:
+    @staticmethod
+    def get_external_temp():
+        """
+        Safely read the external temperature
+        :return: Integer of current temperature
+        """
+        app.logger.info("Starting to read temperature sensor")
+        
+        try:
+            sensor = W1ThermSensor()
+            temperature = sensor.get_temperature()
+        except TypeError as e:
+            app.logger.warning(f"Unable to use primary temperature sensor in this environment: {e}")
+            temperature = 0
+        except Exception as e:
+            app.logger.error(f"Unknown problem with primary external temperature sensor: {e}")
+            temperature = 0
+        
+        app.logger.info("Finished reading temperature sensor")
+        app.logger.debug(f"Temperature: {temperature}")
+        return int(temperature)
+
+                
